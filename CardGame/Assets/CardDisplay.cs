60789b2a7e8caf2e39b9f7c281a5203646a97907
Erikduss <erik.dusschooten@student.hku.nl>
2020-02-28 10:14:29 +0100

    Added Unity project

---
+++ CardGame/Assets/CardDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardDisplay : MonoBehaviour {
+
+	public Card card;
+
+	public TextMesh nameText;
+	public TextMesh descriptionText;
+
+	public SpriteRenderer artworkImage;
+
+	public TextMesh manaText;
+	public TextMesh attackText;
+	public TextMesh healthText;
+
+    public float textSize = 1;
+
+	// Use this for initialization
+	void Start () {
+		nameText.text = card.name;
+        nameText.characterSize = textSize;
+
+		descriptionText.text = card.description;
+
+		artworkImage.sprite = card.artwork;
+
+		manaText.text = card.manaCost.ToString();
+		attackText.text = card.attack.ToString();
+		healthText.text = card.health.ToString();
+	}
+	
+}
